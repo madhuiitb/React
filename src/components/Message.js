9761a4ec0f8dd0edd155fdd1639abfa9d252b206
madhuiitb <54939657+madhuiitb@users.noreply.github.com>
2021-04-03 14:06:28 +0530

    Add files via upload

---
+++ src/components/Message.js
@@ -0,0 +1,26 @@
+import React, {Component, component} from 'react'
+
+class Message extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            message: 'Welcome Visitor'
+        }
+    }
+    changeMessage(){
+        this.setState({
+            message: 'Thank you for subscribing'
+        })
+    }
+    render(){
+        return( 
+            <div>
+                <h1>{this.state.message}</h1>
+                <button onClick = {()=> this.changeMessage()}>Subscribe</button>
+            </div>
+        ) 
+    }
+}
+
+export default Message
